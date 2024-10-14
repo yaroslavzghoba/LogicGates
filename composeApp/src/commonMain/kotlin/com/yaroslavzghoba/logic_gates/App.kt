14b11c79c601e120eee455525aa878a1972b938d
Yaroslav Zghoba <yaroslavzghoba@gmail.com>
2024-10-15 00:10:56 +0200

    Added a foundation for creating applications

---
+++ composeApp/src/commonMain/kotlin/com/yaroslavzghoba/logic_gates/App.kt
@@ -0,0 +1,13 @@
+package com.yaroslavzghoba.logic_gates
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/yaroslavzghoba/logic_gates/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.yaroslavzghoba.logic_gates
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
